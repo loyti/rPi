5d7fc3439871cc68b5dc43deeb1dcfcc77d51f16
loyti <brice@loyti.com>
2017-07-02 16:39:09 -0700

    initial commit

---
+++ pythonPlay/filterStringSum.py
@@ -0,0 +1,24 @@
+spI = ["MJ is GOAT", 8,1,14]
+sS = ["Red Sox Rule", "From Michigan, show the point on your right palm"]
+bS = ["When I write I remeber", "Teach me and I know", "Involve me and I learn"]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+sum = 0
+sumInt = [sum]
+sumString = []
+bigList = [spI, sS, bS, mL, lL]
+for currentTest in bigList:
+	if isinstance(currentTest, list):
+		for itemTest in currentTest:
+			if type(itemTest) is int:
+				sum += itemTest
+				sumInt.append(itemTest)
+				continue
+			elif type(itemTest) is str:
+				print "you found a string"
+				break
+
+bigList = [spI, sS, bS, mL lL]
+for i in bigList:
+	if type(bigList[i]) is int: 
+		print "You found an int"

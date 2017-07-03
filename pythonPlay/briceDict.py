410cb56d6ff02a7648361baf3dfd1228d8865295
loyti <brice@loyti.com>
2017-07-03 14:33:53 -0700

    initial commit

---
+++ pythonPlay/briceDict.py
@@ -0,0 +1,5 @@
+aboutB = {"name":"Brice","age":"33","country":"USA","unspokenLanguage":"Latin"}
+def aboutBrice (aboutB):
+	for keys,vals in aboutB.items():
+		print "My",keys,"is",vals
+aboutBrice(aboutB)

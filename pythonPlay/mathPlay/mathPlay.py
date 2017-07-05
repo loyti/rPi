e9e8acfd417e25c157bb1aeac1643db446db1db8
loyti <brice@loyti.com>
2017-07-05 13:45:36 -0700

    updated py file

--- pythonPlay/mathPlay/mathPlay.py
+++ pythonPlay/mathPlay/mathPlay.py
@@ -1,11 +1,9 @@
-class mathDojo(object):
-	def __init__(self, num1, num2):
-		self.num1 = num1
-		self.num2 = num2
-class add(mathDojo):
-	def __init__(self):
-		superself.result = self.num1 + self.num2
-		print "You just added {} with {} giving you {}".format(self.num1, self.num2, self.result)
+class MathDojo(object):
+	def __init__(self, num*):
+		self.num = num
+	def add(self):
+		self.num =+ self.num
+		print "You just added {} with {} giving you {}".format(self.num)
 		return self
 	def subtract(self):
 		self.result = self.num1 - self.num2

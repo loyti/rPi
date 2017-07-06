898854e1ebf2354033af30fee226120dc68b2df4
loyti <brice@loyti.com>
2017-07-05 18:08:09 -0700

    updated py file

--- pythonPlay/callCenter/callCenter.py
+++ pythonPlay/callCenter/callCenter.py
@@ -1,11 +1,14 @@
 class Call(object):
-	count = 0
+	callID = 0
+	callQue = []
 	def __init__(self):
 		pass
 
 	def newCall(self,callerName,callerNumber,callTime,callReason):
-		self.__class__.count += 1
-		self.id = self.__class__.count
+		self.__class__.callID += 1
+		self.id = self.__class__.callID
+		self.__class__.callQue.append(callerName)
+		self.callQue = self.__class__.callQue
         	self.callerName = callerName
         	self.callerNumber = callerNumber
         	self.callTime = callTime
@@ -14,7 +17,11 @@ class Call(object):
 
 	def displayCall(self):
         	print self.id,self.callerName,self.callerNumber,self.callTime,self.callReason
-        	return self
+        	if len(self.callQue) == 1:
+			print len(self.callQue),"Caller in the queue",self.callQue
+		else:
+			print len(self.callQue),"Callers in the queue",self.callQue
+		return self
 
 randomCall1 = Call()
 randomCall1.newCall("Brice",7145555555,1700,"Pizza Delivery")
@@ -25,17 +32,11 @@ randomCall2.newCall("Niko",6265555555,800,"Breakfast")
 randomCall2.displayCall()
 
 randomCall3 = Call()
-randomCall3.newCall("Kelvin",2135555555,1200,"Lunch")
+randomCall3.newCall("Mark",2135555555,1200,"Lunch")
 randomCall3.displayCall()
 
 randomCall4 = Call()
 randomCall4.newCall("Oli",3105555555,1000,"Films")
 randomCall4.displayCall()
 
-class CallCenter(object):
-	def __init__(self):
-		pass
-	def callTake(self):
-        	pass
-	def callQue(self):
-        	pass
+
